143f403aaa68977429217e305a1aa0d5285a8a2f
rscarz <rosa.scarz@gmail.com>
2024-09-25 09:57:27 -0400

    pagina deploy

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,23 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //res.send(JSON.stringify({users}, null, 4));
+  //return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json(JSON.stringify({books}, null, 4));
+  res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  ///return res.status(300).json({message: "Yet to be implemented"});
+ 
+    const isbn = req.params.isbn;
+    // Filter  
+    books = books.filter((lBook) => books.isbn != isbn);
+    // Send a success message as the response
+    res.send(JSON.stringify({books}, null, 4));
+
  });
   
 // Get book details based on author
